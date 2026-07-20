42ac976c286570a0ccae23ed60dbf756365bf143
LishaZhu1996 <116727653+LishaZhu1996@users.noreply.github.com>
2026-07-20 21:10:38 +0200

    Delete network-pipeline directory

--- network-pipeline/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Make src/ importable in all tests
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
-
-# Default to local filesystem so tests never touch S3
-os.environ.setdefault("USE_LOCAL_FS", "true")
-os.environ.setdefault("BASE_DATA_PATH", "/tmp/network-pipeline-test")
